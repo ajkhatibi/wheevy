9e1b83e2ec096bdda97dac40db3e7df83076d2d0
Julian Khatibi <ajkhatibi@gmail.com>
2017-03-15 20:09:29 -0700

    updated nporgress from appropiate files

--- src/components/pages/dashboard/Overview/index.jsx
+++ src/components/pages/dashboard/Overview/index.jsx
@@ -2,11 +2,11 @@ import NProgress from 'nProgress';
 
 module.exports = {
   path: '/dashboard/overview',
-  getComponent(location, cb) {
-  	NProgress.start();
-    require.ensure([], (require) => {
-    	require('nProgress').done();
-      cb(null, require('./Overview'));
-    });
-  }
+  // getComponent(location, cb) {
+  // 	NProgress.start();
+  //   require.ensure([], (require) => {
+  //   	require('nProgress').done();
+  //     cb(null, require('./Overview'));
+  //   });
+  // }
 }
--- src/components/pages/dashboard/Reports/index.jsx
+++ src/components/pages/dashboard/Reports/index.jsx
@@ -2,11 +2,11 @@ import NProgress from 'nProgress';
 
 module.exports = {
   path: '/dashboard/reports',
-  getComponent(location, cb) {
-  	NProgress.start();
-    require.ensure([], (require) => {
-			require('nProgress').done();
-      cb(null, require('./Reports'));
-    });
-  }
+  // getComponent(location, cb) {
+  // 	NProgress.start();
+  //   require.ensure([], (require) => {
+		// 	require('nProgress').done();
+  //     cb(null, require('./Reports'));
+  //   });
+  // }
 }
