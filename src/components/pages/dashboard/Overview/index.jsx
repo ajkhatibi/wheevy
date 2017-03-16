8e36bcff055364e075c643326ab57e0ed9f15488
Julian Khatibi <ajkhatibi@gmail.com>
2017-03-15 21:38:18 -0700

    replaced previous config settings back to normal 2

--- src/components/pages/dashboard/Overview/index.jsx
+++ src/components/pages/dashboard/Overview/index.jsx
@@ -2,11 +2,11 @@
 
 module.exports = {
   path: '/dashboard/overview',
-  // getComponent(location, cb) {
-  // 	NProgress.start();
-  //   require.ensure([], (require) => {
-  //   	require('nProgress').done();
-  //     cb(null, require('./Overview'));
-  //   });
-  // }
+  getComponent(location, cb) {
+  	NProgress.start();
+    require.ensure([], (require) => {
+    	require('nProgress').done();
+      cb(null, require('./Overview'));
+    });
+  }
 }
